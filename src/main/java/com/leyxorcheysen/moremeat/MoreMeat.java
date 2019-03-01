7a23054eb0000ab40a61ab0cbd631e82541c67ad
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-03-01 21:44:28 +0200

    Added proxy and instance

--- src/main/java/com/leyxorcheysen/moremeat/MoreMeat.java
+++ src/main/java/com/leyxorcheysen/moremeat/MoreMeat.java
@@ -1,8 +1,43 @@
 package com.leyxorcheysen.moremeat;
 
+import com.leyxorcheysen.moremeat.common.CommonProxy;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "moremeat", name = "More Meat", version = "0.0.0", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/LeyxorCheysen/MoreMeat/master/versions.json")
 public class MoreMeat {
+	
+	@Instance
+	public static MoreMeat instance;
+	
+	@SidedProxy(clientSide = "com.leyxorcheysen.moremeat.client.ClientProxy", serverSide = "com.leyxorcheysen.moremeat.common.CommonProxy")
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	    proxy.preInit(event);
+	    
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	    proxy.init(event);
+	    
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	    proxy.postInit(event);
+	    
+	}
 
 }
---
+++ src/main/java/com/leyxorcheysen/moremeat/client/ClientProxy.java
@@ -0,0 +1,32 @@
+package com.leyxorcheysen.moremeat.client;
+
+import com.leyxorcheysen.moremeat.common.CommonProxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ClientProxy extends CommonProxy {
+	
+	@Override
+    public void preInit(FMLPreInitializationEvent event) {
+		
+        super.preInit(event);
+        
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+    	
+        super.init(event);
+        
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+    	
+        super.postInit(event);
+        
+    }
+
+}
