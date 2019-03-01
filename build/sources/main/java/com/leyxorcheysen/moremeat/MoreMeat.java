35e944f25b8daf1edcba3965cc9c8634c63916a8
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-03-01 21:34:09 +0200

    Tests

---
+++ build/sources/main/java/com/leyxorcheysen/moremeat/MoreMeat.java
@@ -0,0 +1,8 @@
+package com.leyxorcheysen.moremeat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "moremeat", name = "More Meat", version = "0.0.0", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/LeyxorCheysen/MoreMeat/master/versions.json")
+public class MoreMeat {
+
+}
